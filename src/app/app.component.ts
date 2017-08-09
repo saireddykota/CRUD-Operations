ec673aac50bbd1c9d08acf42a151ca0f3f1aea79
sai <saireddykota@gmail.com>
2017-08-09 12:42:17 -0500

    update in the array list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-arr =['venkata','sairikb','kumarc'];
+arr =[''];
 
 newItem ="";
+
+//Create
 pushItem = function(){
   if(this.newItem !=""){
     this.arr.push(this.newItem);
     this.newItem ="";
   }
 }
+
+//Update
+updateItem =function(){
+  if (this.newItem !=""){
+    this.arr.update(this.newItem);
+    this.newItem="";
+  }
+}
+
+//Delete
 removeItem = function(index){
 this.arr.splice(index, 1);
 
